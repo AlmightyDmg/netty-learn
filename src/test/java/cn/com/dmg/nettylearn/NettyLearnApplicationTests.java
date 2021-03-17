57e681f69f9373e3156afbe57ff10f7fa0180be5
zhum <zhumeng@skyvis.com.cn>
2021-03-17 10:44:58 +0800

    Initial commit

---
+++ src/test/java/cn/com/dmg/nettylearn/NettyLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.dmg.nettylearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
